b6c006b219b83bf0715ffc2844922fae3c452cc9
Makasharip <murtazaliev002@mail.ru>
2023-11-19 19:38:23 +0300

    Исправил проперти

--- src/test/java/properties/TestConfiguration.java
+++ src/test/java/properties/TestConfiguration.java
@@ -15,11 +15,13 @@ public class TestConfiguration {
         Configuration.remote = System.getProperty("webDriverHost");
         //String testBrowser = System.getProperty("browser", "chrome");
         Configuration.browser = System.getProperty("browser", "chrome");
-        String testVersion = System.getProperty("browserVersion", "113");
-        String testWindowSize = System.getProperty("browserSize", "1920x1080");
+        //String testVersion = System.getProperty("browserVersion", "113");
+        Configuration.browserVersion = System.getProperty("browserVersion", "113");
+       //String testWindowSize = System.getProperty("browserSize", "1920x1080");
+        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
 
-        //Configuration.browser = testBrowser;
-        Configuration.browserSize = testWindowSize;
-        Configuration.browserVersion = testVersion;
+//        //Configuration.browser = testBrowser;
+//        Configuration.browserSize = testWindowSize;
+//        Configuration.browserVersion = testVersion;
     }
 }
